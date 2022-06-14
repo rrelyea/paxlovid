305282c7b980dd5c281ad2adc73543c609d1bb5f
Rob Relyea <rob@relyeas.net>
2022-06-14 12:32:46 -0700

    workaround zipCodes w/o leading zeros. fix chart refresh bug when changing states.

--- src/DoseViewer.js
+++ src/DoseViewer.js
@@ -71,8 +71,19 @@ class DoseViewer extends React.Component {
       })
     }
 
+    fullZip(zip) {
+      switch (zip.length) {
+        case 3:
+          return "00" + zip;
+        case 4:
+          return "0" + zip;
+        default:
+          return zip;
+      }
+    }
+
     async loadDoseInfo() {
-      var doseInfo = await this.toCsv(this.baseUrl + this.props.zipCode + ".csv");
+      var doseInfo = await this.toCsv(this.baseUrl + this.fullZip(this.props.zipCode) + ".csv");
       if (this.state.mounted) {
         this.setState({doseInfo: doseInfo})
       }
@@ -88,110 +99,151 @@ class DoseViewer extends React.Component {
       return dose;
     }
 
-      async componentWillUnmount () {
-        this.state.mounted = false;
-      }
+    async componentWillUnmount () {
+      this.state.mounted = false;
+    }
 
-      async componentDidMount () {
-        this.state.mounted = true;
-        ChartJS.register(LineController, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
-        await this.loadDoseInfo();
-        
-        if (this.state.doseInfo != null) {
-          var j = 0;
-          this.state.chartData.labels[j] = "";
-          this.state.availableData[j] = 0;
-          this.state.noReportsData[j] = 0;
-          j = j + 1;
-        
-          var lastReportDate;
-          var lastPopDataDate;
-          var lastFullReportDate;
-          var lastAvailable;
-          var dosesAdministered;
-          for (var i = 0; i < this.state.doseInfo.length; i++) {
-              var provider = this.state.doseInfo[i][2] !== undefined ? this.state.doseInfo[i][2] : null;
-              var reportDate = this.GetDate(this.state.doseInfo[i][0], 5);
-              var fullReportDate = this.GetDate(this.state.doseInfo[i][0], 0, 10);
-              var popDataDate = this.GetDate(this.state.doseInfo[i][7], 0, 10);
-              var available = this.GetDoses(this.state.doseInfo[i][6]);
+    async componentDidUpdate(prevProps) {
+      var changed = this.props.zipCode !== prevProps.zipCode || this.props.provider !== prevProps.provider;
+        if (changed) {
+          this.state.dosesAdministeredTotal= 0;
+          this.state.firstAdminDate= null;
+          this.state.availableData= [];
+          this.state.noReportsData= [];
+          this.state.doseInfo= null;
+          await this.fetchData();
+        }
+    }
 
-              if (provider != null && provider.toUpperCase() === this.props.provider.toUpperCase() && reportDate !== null) {
-                if (reportDate !== lastReportDate || available !== lastAvailable) {
-                  var dayDiff = this.getDays(new Date(lastReportDate), new Date(reportDate));
-                  dosesAdministered = lastAvailable - available;
-                  if (constantsSite.site === "Evusheld") {
-                    if (dosesAdministered > 0 && dosesAdministered < constantsSite.dosesInBox && available !== null) {
-                      this.state.dosesAdministeredTotal += dosesAdministered;
-                      if (this.state.firstAdminDate === null) {
+    async fetchData() {
+      await this.loadDoseInfo();
+      this.state.chartData=
+      {
+        labels: [],
+        datasets: [{
+          data: [], 
+          label: "Doses Available (in stock)",
+          borderColor: '#3e95cd',
+          backgroundColor: 'lightblue',
+          fill: false,}]
+      };
+      this.state.chartOptions= 
+      {
+          responsive: true,
+          maintainAspectRatio: true,
+          scales: {
+              x: {
+                  ticks: {
+                      font: {
+                          size: 10,
+                          },
+                      display: !(this.props.mini === 'true'),
+                  }
+              }
+          },
+      };
+
+      if (this.state.doseInfo != null) {
+        var j = 0;
+        this.state.chartData.labels[j] = "";
+        this.state.availableData[j] = 0;
+        this.state.noReportsData[j] = 0;
+        j = j + 1;
+      
+        var lastReportDate;
+        var lastPopDataDate;
+        var lastFullReportDate;
+        var lastAvailable;
+        var dosesAdministered;
+        for (var i = 0; i < this.state.doseInfo.length; i++) {
+            var provider = this.state.doseInfo[i][2] !== undefined ? this.state.doseInfo[i][2] : null;
+            var reportDate = this.GetDate(this.state.doseInfo[i][0], 5);
+            var fullReportDate = this.GetDate(this.state.doseInfo[i][0], 0, 10);
+            var popDataDate = this.GetDate(this.state.doseInfo[i][7], 0, 10);
+            var available = this.GetDoses(this.state.doseInfo[i][6]);
+
+            if (provider != null && provider.toUpperCase() === this.props.provider.toUpperCase() && reportDate !== null) {
+              if (reportDate !== lastReportDate || available !== lastAvailable) {
+                var dayDiff = this.getDays(new Date(lastReportDate), new Date(reportDate));
+                dosesAdministered = lastAvailable - available;
+                if (constantsSite.site === "Evusheld") {
+                  if (dosesAdministered > 0 && dosesAdministered < constantsSite.dosesInBox && available !== null) {
+                    this.state.dosesAdministeredTotal += dosesAdministered;
+                    if (this.state.firstAdminDate === null) {
+                      this.state.firstAdminDate = lastFullReportDate;
+                    }
+                  }
+                  else if (dosesAdministered < 0)
+                  {
+                    if (dosesAdministered < -(constantsSite.dosesInBox/2)) {
+                      var boxes = Math.ceil(Math.abs(dosesAdministered) / constantsSite.dosesInBox);
+                      var administeredToday = (boxes * constantsSite.dosesInBox) + dosesAdministered;
+                      this.state.dosesAdministeredTotal += administeredToday;
+                      if (administeredToday > 0 && this.state.firstAdminDate === null) {
                         this.state.firstAdminDate = lastFullReportDate;
                       }
-                    }
-                    else if (dosesAdministered < 0)
-                    {
-                      if (dosesAdministered < -(constantsSite.dosesInBox/2)) {
-                        var boxes = Math.ceil(Math.abs(dosesAdministered) / constantsSite.dosesInBox);
-                        var administeredToday = (boxes * constantsSite.dosesInBox) + dosesAdministered;
-                        this.state.dosesAdministeredTotal += administeredToday;
-                        if (administeredToday > 0 && this.state.firstAdminDate === null) {
-                          this.state.firstAdminDate = lastFullReportDate;
-                        }
-                      } 
-                    }
+                    } 
                   }
-
-                  this.state.chartData.labels[j] = reportDate;
-                  if (lastPopDataDate !== popDataDate) {
-                    this.state.availableData[j] = available;
-                  }
-
-                  this.state.noReportsData[j] = available;
-
-                  j = j + 1;
-                  lastReportDate = reportDate;
-                  lastPopDataDate = popDataDate;
-                  lastFullReportDate = fullReportDate;
-                  lastAvailable = available;
                 }
-              }
-          }
-          
-          if (this.props.showChart !== 'false') {
-            this.state.chartData.datasets = [{
-              data: this.state.availableData,
-              label: this.props.mini !== 'true' ? "Doses Reported (in stock)" : "Doses",
-              borderColor: this.props.dataDate !== null ? '#ffa500' : '#00DD00',
-              backgroundColor: this.props.dataDate !== null ? '#ffa500' : '#00DD00',
-              fill: false,
-            },
-            {
-              data: this.state.noReportsData,
-              label: this.props.mini !== 'true' ? "No Updates" : "No Updates",
-              borderColor: this.props.dataDate !== null ? '#616161' : '#616161',
-              pointRadius: '0',
-              backgroundColor: this.props.dataDate !== null ? '#616161' : '#616161',
-              fill: false,
-            }];
-          }
 
-          if (this.state.mounted) {
-            this.setState({chartData:this.state.chartData});
-          }
+                this.state.chartData.labels[j] = reportDate;
+                if (lastPopDataDate !== popDataDate) {
+                  this.state.availableData[j] = available;
+                }
+
+                this.state.noReportsData[j] = available;
+
+                j = j + 1;
+                lastReportDate = reportDate;
+                lastPopDataDate = popDataDate;
+                lastFullReportDate = fullReportDate;
+                lastAvailable = available;
+              }
+            }
+        }
+        
+        if (this.props.showChart !== 'false') {
+          this.state.chartData.datasets = [{
+            data: this.state.availableData,
+            label: this.props.mini !== 'true' ? "Doses Reported (in stock)" : "Doses",
+            borderColor: this.props.dataDate !== null ? '#ffa500' : '#00DD00',
+            backgroundColor: this.props.dataDate !== null ? '#ffa500' : '#00DD00',
+            fill: false,
+          },
+          {
+            data: this.state.noReportsData,
+            label: this.props.mini !== 'true' ? "No Updates" : "No Updates",
+            borderColor: this.props.dataDate !== null ? '#616161' : '#616161',
+            pointRadius: '0',
+            backgroundColor: this.props.dataDate !== null ? '#616161' : '#616161',
+            fill: false,
+          }];
+        }
+
+        if (this.state.mounted) {
+          this.setState({chartData:this.state.chartData});
         }
       }
+    }
 
+    async componentDidMount () {
+      this.state.mounted = true;
+      ChartJS.register(LineController, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+      await this.fetchData();
+    }
 
     getDoses () {
         return this.state.doseInfo[3][2];
     }
+
     getDays(day1, day2) {
       day1.setHours(0,0,0,0);
       day2.setHours(0,0,0,0);
       return (+day2 - +day1)/this.msInDay
     }
+
     render() {
         var dayDiff = this.getDays(new Date(this.state.firstAdminDate), new Date());
-
         return (
         <>
           <div id='doses'>
@@ -212,7 +264,7 @@ class DoseViewer extends React.Component {
             : false }
           </div>
           { this.props.mini !== 'true' ? 
-          <div><br/><a href={this.baseUrl + this.props.zipCode + ".csv"}>raw inventory data ({this.props.zipCode+".csv"})</a></div>
+          <div><br/><a href={this.baseUrl + this.fullZip(this.props.zipCode) + ".csv"}>raw inventory data ({this.fullZip(this.props.zipCode)+".csv"})</a></div>
            : false}
         </>
         );
